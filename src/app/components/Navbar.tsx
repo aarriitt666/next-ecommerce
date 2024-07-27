3d55c1597d3f856df2ec998911d7d55593fd5419
Vinh Nguyen <argh2xxx@gmail.com>
2024-07-27 14:42:10 -0400

    First commit after making changes to starter.

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Menu from "./Menu";
+import Image from "next/image";
+import SearchBar from "./SearchBar";
+import NavIcons from "./NavIcons";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* MOBILE */}
+      <div className="h-full flex items-center justify-between md:hidden">
+        <Link href="/">
+          <div className="text-2xl tracking-wide">Haodexino</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center justify-between h-full">
+        {/* LEFT */}
+        <div className="flex items-center gap-6">
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/logo.png" alt="Logo" width={24} height={24} />
+            <div className="text-2xl tracking-wide">Haodexino</div>
+          </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+            <Link href="/">Logout</Link>
+            <Link href="/" className="mr-4">
+              Cart(1)
+            </Link>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="flex items-center gap-4 ml-auto">
+          <SearchBar />
+          <NavIcons />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
