9b4f8c9aebdebc1794282fbbfe6dbcdad0403a42
Vinh Nguyen <argh2xxx@gmail.com>
2024-08-15 00:12:51 -0400

    Design for single product page is done now working on the components within single product page.

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,13 +1,39 @@
+import Add from "../components/Add";
+import CustomizeProducts from "../components/CustomizeProducts";
 import ProductImages from "../components/ProductImages";
 
 const SinglePage = () => {
   return <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
     {/* IMG  */}
-    <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
+    <div className='w-full lg:w-1/2 lg:sticky top-0 h-max'>
       <ProductImages />
     </div>
     {/* TEXTS */}
-    <div className='w-full lg:w-1/2 flex flex-col gap-6'>TEXTS</div>
+    <div className='w-full lg:w-1/2 flex flex-col gap-6'>
+      <h1 className="text-4xl font-medium">Product Name</h1>
+      <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et totam suscipit aliquam blanditiis repudiandae sequi natus quas tempore, delectus reiciendis soluta neque vel consequuntur impedit dolores ut aspernatur nemo unde.</p>
+      <div className='h-[2px] bg-gray-100' />
+      <div className='flex items-center gap-4'>
+        <h3 className="text-xl text-gray-500 line-through">$59</h3>
+        <h2 className="font-medium text-2xl">$49</h2>
+      </div>
+      <div className='h-[2px] bg-gray-100' />
+      <CustomizeProducts />
+      <Add />
+      <div className='h-[2px] bg-gray-100' />
+      <div className='text-sm'>
+        <h4 className="font-medium mb-4">Title</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, maiores, quae officia quas accusamus enim quam est, possimus vitae fugiat hic sunt commodi aut minus labore modi assumenda corporis repellendus.</p>
+      </div>
+      <div className='text-sm'>
+        <h4 className="font-medium mb-4">Title</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, maiores, quae officia quas accusamus enim quam est, possimus vitae fugiat hic sunt commodi aut minus labore modi assumenda corporis repellendus.</p>
+      </div>
+      <div className='text-sm'>
+        <h4 className="font-medium mb-4">Title</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, maiores, quae officia quas accusamus enim quam est, possimus vitae fugiat hic sunt commodi aut minus labore modi assumenda corporis repellendus.</p>
+      </div>
+    </div>
   </div>;
 };
 
---
+++ src/app/components/Add.tsx
@@ -0,0 +1,8 @@
+
+const Add = () => {
+    return (
+        <div className=''>Add</div>
+    );
+};
+
+export default Add;
